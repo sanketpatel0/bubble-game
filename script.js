c3b8d85962e539d4f4a6ef6df6bfbb471eef00ef
sanketpatel0 <152383553+sanketpatel0@users.noreply.github.com>
2023-12-12 19:17:59 +0530

    add index.html

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+const bubble = () => {
+    var clutter = " ";
+
+    for (var a = 1; a <= 116; a++) {
+
+        var num = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${num}</div>`
+    }
+
+    document.querySelector(".p-btm").innerHTML = clutter;
+
+}
+
+
+var timer = 60;
+const runtimer = () => {
+    setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector('#timerval').textContent = timer;
+        } else {
+            timer = 0;
+            document.querySelector('.p-btm').innerHTML = `<h1>The Game Is Over</h1>  <h2>Your Score Is :- ${score}</h2>`;
+        }
+    }, 1000);
+}
+
+
+var newh = 0;
+const newHit = () => {
+
+    newh = Math.floor(Math.random() * 10);
+
+    document.getElementById('hitval').textContent = newh;
+}
+
+
+var score = 0;
+function incScore() {
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+
+
+bubble();
+runtimer();
+newHit();
+
+document.querySelector('.p-btm').addEventListener("click", function (d) {
+    var clicked = Number(d.target.textContent)
+
+    if (clicked === newh) {
+        incScore();
+        bubble();
+        newHit();
+    }
+})
+
+function clicked() {
+    timer = 60;
+    newHit();
+    var upScore = 0;
+    document.querySelector("#scoreval").textContent = upScore;
+    var clutter = " ";
+
+    for (var a = 1; a <= 116; a++) {
+
+        var num = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${num}</div>`
+    }
+
+    document.querySelector(".p-btm").innerHTML = clutter;
+}
+
+
+
